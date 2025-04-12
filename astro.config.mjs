df79fe5051363e45a22803d30f17f0f2c398b470
castillo ignacio <castilloignacio1510@gmail.com>
2025-04-12 19:00:39 -0300

    Creando la animación para mover a los jugadores

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
 import AstroPWA from '@vite-pwa/astro';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   integrations: [AstroPWA()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
