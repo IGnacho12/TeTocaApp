1dbde0016d3ba545ae1020e5c311124b0e91498e
nacho <castilloignaio1510@gmail.com>
2025-04-28 14:41:19 -0300

    Cachenado fuente

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,12 @@
 self.addEventListener("install", event => {
   console.log("Service Worker instalado");
-  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/public/font.otf'
+      ]);
+    })
+  );
 });
 
 self.addEventListener("activate", event => {
@@ -13,4 +19,11 @@ self.addEventListener("fetch", event => {
       return response || fetch(event.request);
     })
   );
+});
+
+
+caches.open("pwa-assets")
+.then(cache => {
+  cache.add("styles.css"); // it stores only one resource
+  cache.addAll(["styles.css", "app.js"]); // it stores two resources
 });
